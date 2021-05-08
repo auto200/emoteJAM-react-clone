03dd2a0135f1f6d550f9da3068d5fe0c36ec9853
auto200 <michal.warac@gmail.com>
2021-05-08 12:34:49 +0200

    Set dark theme as default

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,10 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
-
-const fonts = { mono: `'Menlo', monospace` }
-
-const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  colors: {
-    black: '#16161D',
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
   },
-  fonts,
-  breakpoints,
-})
+});
 
-export default theme
+export default theme;
